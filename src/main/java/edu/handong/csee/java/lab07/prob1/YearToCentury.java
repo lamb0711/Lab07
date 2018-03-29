999bd17a2a865814336477abcdb2fb9acafcc9be
Yang Sujin <21500394@handong.edu>
2018-03-29 22:22:45 +0900

    add basic variable

--- src/main/java/edu/handong/csee/java/lab07/prob1/YearToCentury.java
+++ src/main/java/edu/handong/csee/java/lab07/prob1/YearToCentury.java
@@ -1,9 +1,14 @@
 package edu.handong.csee.java.lab07.prob1;
-
+import java.util.Scanner;
 public class YearToCentury {
+	int divYear;
+	int modYear;
+	int yr;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int year;
+		
 
 	}
 
