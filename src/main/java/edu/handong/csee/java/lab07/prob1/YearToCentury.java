b72796c7a3dd5303cb2ea6016fac8d49b6994376
Yang Sujin <21500394@handong.edu>
2018-03-29 22:31:51 +0900

    save input data and add calculate methods

--- src/main/java/edu/handong/csee/java/lab07/prob1/YearToCentury.java
+++ src/main/java/edu/handong/csee/java/lab07/prob1/YearToCentury.java
@@ -4,19 +4,29 @@ public class YearToCentury {
 	int divYear;
 	int modYear;
 	int yr;
+	int century;
 	
 	public void setter(int year) {
 		yr = year;
+	}
+	
+	public int WhatCentury() {
+		
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int year;
 		
-		Scanner Keyboard = new Scanner(System.in);
+		Scanner keyboard = new Scanner(System.in);
 		
 		YearToCentury ytc = new YearToCentury();
 		
+		System.out.println("Input Year: ");
+		year = keyboard.nextInt();
+		ytc.setter(year);
+		
+		System.out.println(year+" is "+WhatCentury()+"th century.");
 		
 		
 
