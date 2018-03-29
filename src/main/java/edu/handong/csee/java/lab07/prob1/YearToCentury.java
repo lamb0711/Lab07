a5fafb2fd5628680b8ea37135e76a1fffdd48594
Yang Sujin <21500394@handong.edu>
2018-03-29 22:26:33 +0900

    add constructor

--- src/main/java/edu/handong/csee/java/lab07/prob1/YearToCentury.java
+++ src/main/java/edu/handong/csee/java/lab07/prob1/YearToCentury.java
@@ -4,10 +4,20 @@ public class YearToCentury {
 	int divYear;
 	int modYear;
 	int yr;
+	
+	public void setter(int year) {
+		yr = year;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int year;
+		
+		Scanner Keyboard = new Scanner(System.in);
+		
+		YearToCentury ytc = new YearToCentury();
+		
+		
 		
 
 	}
