f633fd2209f55b3fe920a50f1aa3c40e8ec92834
Yang Sujin <21500394@handong.edu>
2018-04-03 08:54:05 +0900

    make set method and no input method

--- src/main/java/edu/handong/csee/java/lab07/prob2/Chicken.java
+++ src/main/java/edu/handong/csee/java/lab07/prob2/Chicken.java
@@ -1,6 +1,25 @@
 package edu.handong.csee.java.lab07.prob2;
 
 public class Chicken {
+	public static String name;
+	public static double price;
+	public static int star;
+	
+	public void Chicken() {
+		name = "";
+		price = 0.00;
+		star = 0;
+	}
+	
+	public void Chicken(String n, double p, int s) {
+		this.name = n;
+		this.price = p;
+		this.star = s;
+	}
+	
+	
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
