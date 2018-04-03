5ebf0092b93aca4e550f6ba69ced2cddcec3823b
Yang Sujin <21500394@handong.edu>
2018-04-03 09:53:48 +0900

    add get and set methods

--- src/main/java/edu/handong/csee/java/prob3/Attendance.java
+++ src/main/java/edu/handong/csee/java/prob3/Attendance.java
@@ -3,20 +3,20 @@ import java.util.Random;
 public class Attendance {
 	public String name;
 	public int year;
-	public int sutNum;
+	public int stdNum;
 	public int absence;
 	
 	public Attendance() {
 		this.name = "";
 		this.year = 0;
-		this.sutNum = 0;
+		this.stdNum = 0;
 		this.absence = 0;
 	}
 	
 	public Attendance(String n, int y, int s) {
 		this.name = n;
 		this.year = y;
-		this.sutNum = s;
+		this.stdNum = s;
 	}
 	
 	public void randAbsence() {
@@ -28,6 +28,30 @@ public class Attendance {
 	public void compareAbsence() {
 		if(this.absence > 6)
 			System.out.println("¡°I¡¯m sorry, "+this.name+". You failed this course");
+	}
+	
+	public String getName() {//return name variable
+		return this.name;//return name
+	}
+
+	public int getYear() {//return year variable
+		return this.year;//return year
+	}
+
+	public int getStdNum() {//return student number variable
+		return this.stdNum;//return stdNum
+	}
+
+	public void setName(String n) {//save input data to name variable
+		this.name = n;//save n to name
+	}
+
+	public void setYear(int y) {//save input data to year variable
+		this.year = y;//save y to year
+	}
+
+	public void setStdNum(int s) {//save input data to stdNum variable
+		this.stdNum = s;//save s to stdNum
 	}
 
 	public static void main(String[] args) {
