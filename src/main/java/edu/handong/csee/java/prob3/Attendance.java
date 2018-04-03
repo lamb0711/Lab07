6a69a55770bfb9cd2512ace5d41fda86cdff277b
Yang Sujin <21500394@handong.edu>
2018-04-03 10:30:24 +0900

    arrange line

--- src/main/java/edu/handong/csee/java/prob3/Attendance.java
+++ src/main/java/edu/handong/csee/java/prob3/Attendance.java
@@ -16,37 +16,37 @@ public class Attendance {//Attendance class
 	public int year;//save student year
 	public int stdNum;//save student number
 	public int absence;//save student absence
-	
+
 	public Attendance() {// if input data is not found, implement this method.
 		this.name = "";//no name
 		this.year = 0;//0 year
 		this.stdNum = 0;//0 student number
 		this.absence = 0;//0 absence
 	}
-	
+
 	public Attendance(String n, int y, int s, int a) {//if input data exist, implement this method.
 		this.name = n;//save input data to name
 		this.year = y;//save input data to year
 		this.stdNum = s;//save input data to stdNum
 		this.absence = a;//save input data to absence
 	}
-	
+
 	public void randAbsence() {//save random number to absence variable(0~10) 
 		Random randomGen = new Random();//use random class so declare random instance
 		int randomInt = randomGen.nextInt(10);//declare randmInt variable and save random number
 		this.absence = randomInt;//save random number to absence
 	}
-	
+
 	public void compareAbsence() {//if student absence is greater than 6, print fail message. if not print grade message.
 		if(this.absence > 6) {//if absence is greater than 6, print fail message
 			System.out.println("¡°I¡¯m sorry, "+this.name+". You failed this course");//fail message
-		    System.out.println(this.name + " - Number of absence: " + this.absence);//fail message and absence
+			System.out.println(this.name + " - Number of absence: " + this.absence);//fail message and absence
 		}
-		    else//if absence is not greater than 6
+		else//if absence is not greater than 6
 			System.out.println("We'll see about the grade, " + this.name);//print grade message and name
 
 	}
-	
+
 	public String getName() {//return name variable
 		return this.name;//return name
 	}
@@ -58,7 +58,7 @@ public class Attendance {//Attendance class
 	public int getStdNum() {//return student number variable
 		return this.stdNum;//return stdNum
 	}
-	
+
 	public int getAbsence() {//return student absence variable
 		return this.absence;//return absence
 	}
@@ -74,7 +74,7 @@ public class Attendance {//Attendance class
 	public void setStdNum(int s) {//save input data to stdNum variable
 		this.stdNum = s;//save s to stdNum
 	}
-	
+
 	public void setAbsence(int a) {//save input data to absence variable
 		this.absence = a;//save a to absence
 	}
@@ -85,20 +85,20 @@ public class Attendance {//Attendance class
 		Attendance s_2 = new Attendance();//declare Attendance instance variable and empty data
 		Attendance s_3 = new Attendance("Lucas", 1, 21833222, 0);//declare Attendance instance variable and initialize data
 		Attendance s_4 = new Attendance("Martha", 2, 21733444, 0);//declare Attendance instance variable and initialize data
-		
+
 		s_1.setName("Jess"); s_1.setYear(4); s_1.setStdNum(21400999); s_1.setAbsence(0);//save s_1 data to use set methods
 		s_2.setName("Kent"); s_2.setYear(2); s_2.setStdNum(21700111); s_2.setAbsence(0);//save s_2 data to use set methods
-		
+
 		s_1.randAbsence();//save random number to s_1.absence variable
 		s_2.randAbsence();//save random number to s_2.absence variable
 		s_3.randAbsence();//save random number to s_3.absence variable
 		s_4.randAbsence();//save random number to s_4.absence variable
-		
+
 		s_1.compareAbsence();//call compareAbsence method and print message
 		s_2.compareAbsence();//call compareAbsence method and print message
 		s_3.compareAbsence();//call compareAbsence method and print message
 		s_4.compareAbsence();//call compareAbsence method and print message
-		
+
 	}
 
 }
