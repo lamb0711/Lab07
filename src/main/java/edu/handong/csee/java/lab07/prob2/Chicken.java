5dcc08165e9d82f89ac0b52fc48bfa85b1656de8
Yang Sujin <21500394@handong.edu>
2018-04-03 09:02:31 +0900

    error is formed.. delete static

--- src/main/java/edu/handong/csee/java/lab07/prob2/Chicken.java
+++ src/main/java/edu/handong/csee/java/lab07/prob2/Chicken.java
@@ -1,9 +1,9 @@
 package edu.handong.csee.java.lab07.prob2;
 
 public class Chicken {
-	public static String name;
-	public static double price;
-	public static int star;
+	public String name;
+	public double price;
+	public int star;
 	
 	public void Chicken() {
 		name = "";
@@ -44,6 +44,14 @@ public class Chicken {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	Chicken menu1 = new Chicken("Cheese_mustard", 16000.00, 5);
+	Chicken menu2 = new Chicken("Honey_mustard", 16000.00, 5);
+	Chicken menu3 = new Chicken("Spicey_chicken", 16000.00, 1);
+
+	menu1.set_stars(3);
+	menu2.set_stars(4);
+	menu3.set_stars(1);
+
 
 	}
 
